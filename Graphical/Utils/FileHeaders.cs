9e99f47aa41b7dfd512c3ca2c8883a6fa72dd318
Moling <clovermoling@gmail.com>
2023-01-01 22:03:23 +0800

    Add Graphical

---
+++ Graphical/Utils/FileHeaders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphical.Utils
+{
+    internal class FileHeaders
+    {
+        public Dictionary<string, string> Headers = new Dictionary<string, string>()
+        {
+            { "504B0304", "zip" }
+        };
+    }
+}
