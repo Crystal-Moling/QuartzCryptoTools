31ffc7a489e0530d57d2b1a4e80d0bded885b0f3
Moling <clovermoling@gmail.com>
2023-01-03 15:44:20 +0800

    Add File Hex Reader

--- Graphical/MainWindow.xaml.cs
+++ Graphical/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Panuon.WPF.UI;
+﻿using Graphical.Utils;
+using Microsoft.Win32;
+using Panuon.WPF.UI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -25,67 +27,131 @@ namespace Graphical
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext= this;
+            this.DataContext = new DataBind();
+            WindowOverlayer.Show_StartUp();
         }
 
-        private void SwitchTo_HexViewer(object sender, RoutedEventArgs e)
-        { MainCarousel.CurrentIndex = 1; }
+        private void Overlayer_MouseMove(object sender, MouseEventArgs e)
+        { if (e.LeftButton == MouseButtonState.Pressed) { DragMove(); } }
 
-        public static void Show_LoadingOverlayer()
+        #region 开屏弹窗部分代码
+
+        private void StartUp_ExitApplication(object sender, RoutedEventArgs e)
+        { Close(); }
+
+        private void StartUp_BrowseFile(object sender, RoutedEventArgs e)
         {
-            OverlayerVisible = true;
-            StartUpOverlayerVisible = Visibility.Collapsed;
-            LoadingOverlayerVisible = Visibility.Visible;
+            OpenFileDialog selFile = new OpenFileDialog
+            { Multiselect = false, Title = "请选择文件夹", Filter = "所有文件(*.*)|*.*" };
+            if ((bool)selFile.ShowDialog())
+            { DataBind.FileLocation = selFile.FileName; }
         }
 
-        public static void Show_StartUpOverlayer()
+        private void StartUp_LoadFile(object sender, RoutedEventArgs e)
         {
-            OverlayerVisible = true;
-            StartUpOverlayerVisible = Visibility.Visible;
-            LoadingOverlayerVisible = Visibility.Collapsed;
+            if (DataBind.FileLocation == "")
+            { DataBind.FileLocationBorder = DataBind.FileLocationColor = new SolidColorBrush(Color.FromArgb(255, 255, 119, 119)); }
+            else
+            { Load_FileInfo(DataBind.FileLocation); }
         }
-
-        #region DataBinds
-
-            #region MainWindow
-
-            public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
-
-            private static bool _overlayerVisible = false;
-            public static bool OverlayerVisible
-            {
-                get => _overlayerVisible;
-                set
-                {
-                    _overlayerVisible = value;
-                    StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(OverlayerVisible)));
-                }
-            }
-
-            private static Visibility _loadingOverlayerVisible = Visibility.Collapsed;
-            public static Visibility LoadingOverlayerVisible
-            {
-                get => _loadingOverlayerVisible;
-                set
-                {
-                    _loadingOverlayerVisible = value;
-                    StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(LoadingOverlayerVisible)));
-                }
-            }
-
-            private static Visibility _startUpOverlayerVisible = Visibility.Collapsed;
-            public static Visibility StartUpOverlayerVisible
-            {
-                get => _startUpOverlayerVisible;
-                set
-                {
-                    _startUpOverlayerVisible = value;
-                    StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(StartUpOverlayerVisible)));
-                }
-            }
-
-            #endregion
 
         #endregion
+
+        #region 主页面部分代码
+
+        private void Load_FileInfo(String path)
+        {
+            WindowOverlayer.Hide_StartUp();
+        }
+
+        private void SwitchTo_HexViewer(object sender, RoutedEventArgs e) { MainCarousel.CurrentIndex = 1; }
+
+        #endregion
+    }
+
+    public class DataBind //数据绑定类
+    {
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
+
+        public static bool OverlayerVisible
+        {
+            get => _overlayerVisible;
+            set
+            {
+                _overlayerVisible = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(OverlayerVisible)));
+            }
+        }
+        private static bool _overlayerVisible = false;
+
+        public static Visibility LoadingOverlayerVisible
+        {
+            get => _loadingOverlayerVisible;
+            set
+            {
+                _loadingOverlayerVisible = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(LoadingOverlayerVisible)));
+            }
+        }
+        private static Visibility _loadingOverlayerVisible = Visibility.Collapsed;
+
+        public static Visibility StartUpOverlayerVisible
+        {
+            get => _startUpOverlayerVisible;
+            set
+            {
+                _startUpOverlayerVisible = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(StartUpOverlayerVisible)));
+            }
+        }
+        private static Visibility _startUpOverlayerVisible = Visibility.Collapsed;
+
+        public static String FileLocation
+        {
+            get => _fileLocation;
+            set
+            {
+                _fileLocation = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(FileLocation)));
+            }
+        }
+        private static String _fileLocation = "";
+
+        public static Brush FileLocationBorder
+        {
+            get => _fileLocationBorder;
+            set
+            {
+                _fileLocationBorder = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(FileLocationBorder)));
+            }
+        }
+        private static Brush _fileLocationBorder = new SolidColorBrush(Color.FromArgb(255, 187, 187, 187));
+
+        public static Brush FileLocationColor
+        {
+            get => _fileLocationColor;
+            set
+            {
+                _fileLocationColor = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(FileLocationColor)));
+            }
+        }
+        private static Brush _fileLocationColor = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+    }
+
+    public class WindowOverlayer //遮罩层操作类
+    {
+        public static void Show_Loading()
+        { DataBind.OverlayerVisible = true; DataBind.LoadingOverlayerVisible = Visibility.Visible; }
+
+        public static void Hide_Loading()
+        { DataBind.OverlayerVisible = false; DataBind.LoadingOverlayerVisible = Visibility.Collapsed; }
+
+        public static void Show_StartUp()
+        { DataBind.OverlayerVisible = true; DataBind.StartUpOverlayerVisible = Visibility.Visible; }
+
+        public static void Hide_StartUp()
+        { DataBind.OverlayerVisible = false; DataBind.StartUpOverlayerVisible = Visibility.Collapsed; }
     }
 }
---
+++ Graphical/Utils/HexHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Graphical.Utils
+{
+    internal class HexHelper
+    {
+        public static List<String> ReadHex(String path)
+        {
+            List<String> fullStream = new List<String>();
+            try
+            {
+                FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                long nBytesToRead = stream.Length;
+                byte[] Buffer = new byte[nBytesToRead];
+                int m = stream.Read(Buffer, 0, Buffer.Length);
+                stream.Close();
+                for (int i = 0; i < Buffer.Length; i++)
+                { fullStream.Add(Buffer[i].ToString("X2")); }
+            }
+            catch (Exception) { }
+            return fullStream;
+        }
+
+        public static void WriteHex(String path, List<String> hex)
+        {
+            byte[] Buffer = new byte[hex.Count];
+            for (int i = 0; i < hex.Count; i++)
+            { Buffer[i] = Convert.ToByte(hex[i], 16); }
+            FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+            BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write(Buffer, 0, Buffer.Length);
+            writer.Close();
+            stream.Close();
+        }
+    }
+}
